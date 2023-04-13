6eb3cf4b93f7298e68b83ce9d6eb3c5fdf1a44e4
eduard <eduardogsech@gmail.com>
2023-04-13 14:46:46 -0500

    deploy

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from config.database import engine, Base
 from middlewares.error_handler import ErrorHandler
 from routers.movie import movie_router
 from routers.user import user_router
+import os
+import uvicorn
 app = FastAPI(
     title="My FastAPI's application",
     description="prima api con python :D (de peliculas)",
@@ -53,3 +55,7 @@ movies = [
 @app.get('/', tags=['home'])
 def message():
     return HTMLResponse('<h1>Hello world</h1>')
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0",
+                port=int(os.environ.get("PORT",8000)))
